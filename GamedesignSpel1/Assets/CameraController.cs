a622f57cc45b4ff0595f7bacc6d33f919df358e3
Jordi Martijn van Os <JM.vanOs@student.han.nl>
2019-02-12 15:57:57 +0100

    Adds jumping, camera following, invisible walls

---
+++ GamedesignSpel1/Assets/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float heightOffset = 2.0f;
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position = player.transform.position;
+		transform.position = new Vector3(
+			player.transform.position.x,
+			heightOffset,
+			player.transform.position.z);
+		transform.Translate(Vector3.back * 20.0f);
+	}
+}
--- GamedesignSpel1/Assets/PlayerController.cs
+++ GamedesignSpel1/Assets/PlayerController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
+	
 	public float movementSpeed = 10.0f;
+	public float jumpForce = 10.0f;
 
 	private Rigidbody rb;
 	private Quaternion cameraRotation = Quaternion.Euler(0, 45, 0);
+	private int score = 0;
+
+	public int getScore() {
+		return score;
+	}
+
+	public void addScore(int score) {
+		this.score += score;
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +29,13 @@ public class PlayerController : MonoBehaviour {
 		float dy = Input.GetAxis("Vertical");
 
 		rb.AddForce(cameraRotation * new Vector3(dx, 0.0f, dy) * movementSpeed);
+
+		if (Input.GetKeyDown("space")) {
+			bool isOnFloor = Physics.Raycast(transform.position, Vector3.down, transform.localScale.y);
+			if (isOnFloor) {
+				rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+			}
+		}
 	}
 	
 	// Update is called once per frame
